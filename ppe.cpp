a516f37ef889f9a5f0aeaf0ea421597b3dcf0204
lucas8 <luc.linux@mailoo.org>
2014-04-05 16:08:20 +0200

    Improved protocol.

--- ppe.cpp
+++ ppe.cpp
@@ -12,26 +12,25 @@ void loop()
 {
     if(Serial.available()) {
         /* Parsing command. */
-        char c = Serial.read();
+        char cg = Serial.read();
+        unsigned char c = cg;
         /* Write back, and add the result of parsing. */
         Serial.write(c);
-        if(c & 0xf0 == 0xf0) { /* Turn order. */
-            if(c & 0x0f == 0x01)
-                Serial.println("Turning left !");
-            else
-                Serial.println("Turning right !");
+        if((c & 0xF0) == 0x50) {
+            c &= 0x0F;
+            switch(c) {
+                case 0x00: Serial.print(" Turning left ");   break;
+                case 0x01: Serial.print(" Turning right ");  break;
+                case 0x02: Serial.print(" Stopping ");       break;
+                case 0x03: Serial.print(" Moving forward "); break;
+                case 0x04: Serial.print(" Alert ! ");        break;
+                default:   Serial.print(" Invalid ");        break;
+            }
         }
-        else if(c & 0xa0 == 0xa0) { /* Move order. */
-            if(c & 0x0f == 0x01)
-                Serial.println("Going forward !");
-            else if(c & 0x0f == 0x00)
-                Serial.println("Stopping !");
-        }
-        else if(c == 0x00) { /* Alert order. */
-            Serial.println("Alert !");
-        }
+        else
+            Serial.print(" Wrong format ");
     }
-    
+
     /* TODO write incoming bluetooth data to Serial. */
 }
 
